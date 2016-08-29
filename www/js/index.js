f9ce43bc0e959da605fa6308827d153f4ce6cd86
fabio franci <fabio.franci@gmail.com>
2016-08-29 17:24:04 +0200

    Vai

--- www/js/index.js
+++ www/js/index.js
@@ -172,6 +172,22 @@ var app = {
 
         inizializzazione_variabili();
 
+        function playAudio(encodedUrl) {
+            url=Base64.decode(encodedUrl);
+            console.log(my_media);
+            if (my_media) {
+                my_media.pause();
+            } else {
+                my_media = new Media(url,
+                    // success callback
+                    function () { console.log("playAudio():Audio Success"); },
+                    // error callback
+                    function (err) { console.log("playAudio():Audio Error: " + err); }
+                );
+                my_media.play();
+            }
+        }
+
         var initial = '#page-index';
         if(window.localStorage.getItem('idUser')>0) {
             initial = '#page-index-logged';
