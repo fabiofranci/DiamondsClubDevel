66dc17ab4839d61172037d7338c8869733cbeba3
fabio franci <fabio.franci@gmail.com>
2016-08-29 17:30:46 +0200

    Vai

--- www/js/index.js
+++ www/js/index.js
@@ -39,6 +39,9 @@
  page B---pageshow
 
  */
+
+
+//super globals variables
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 var my_media=null;
 
@@ -622,7 +625,7 @@ var app = {
                 url: "https://www.diamondsclub.it/api/materiali.php",
                 data: jQuery.param(params) ,
                 success: function (data) {
-                    //alert("SUCCESS!");
+                    console.log("SUCCESS!");
                     resp=data.resp;
                     sottocat=data.sottocategorie;
                     md5_materiali=data.md5;
@@ -632,7 +635,7 @@ var app = {
                     window.localStorage.setItem("sottocategorie_materiali_memoria",JSON.stringify(sottocat));
                 },
                 error: function (e) {
-                    //alert("Connessione assente oppure nessun aggiornamento, uso i dati in memoria!");
+                    console.log("Connessione assente oppure nessun aggiornamento, uso i dati in memoria!");
                     //console.log(e);
                     resp=JSON.parse(window.localStorage.getItem("materiali_memoria"));
                     sottocat=JSON.parse(window.localStorage.getItem("sottocategorie_materiali_memoria"));
