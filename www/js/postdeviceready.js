b1c9ef2d62fc76234c30f0793944d9537bd51e7e
fabio franci <fabio.franci@gmail.com>
2016-08-29 17:26:44 +0200

    Vai

--- www/js/postdeviceready.js
+++ www/js/postdeviceready.js
@@ -1,15 +1,20 @@
-    // function playAudio(encodedUrl) {
-    //     url=Base64.decode(encodedUrl);
-    //     console.log(my_media);
-    //     if (my_media) {
-    //         my_media.pause();
-    //     } else {
-    //         my_media = new Media(url,
-    //             // success callback
-    //             function () { console.log("playAudio():Audio Success"); },
-    //             // error callback
-    //             function (err) { console.log("playAudio():Audio Error: " + err); }
-    //         );
-    //         my_media.play();
-    //     }
-    // }
+    var my_media=null;
+
+    inizializzazione_variabili();
+
+    function playAudio(encodedUrl) {
+        url=Base64.decode(encodedUrl);
+        console.log(my_media);
+        if (my_media) {
+            my_media.pause();
+        } else {
+            my_media = new Media(url,
+                // success callback
+                function () { console.log("playAudio():Audio Success"); },
+                // error callback
+                function (err) { console.log("playAudio():Audio Error: " + err); }
+            );
+            my_media.play();
+        }
+    }
+
